1498b17f36cf168aad06d89ad855aae83ed66541
Ramos Felipe <Felipeofor@gmail.com>
2021-12-23 13:42:27 -0300

    calculadora de interes compuesto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import Input from './components/Input';
+import Button from './components/Button.js';
+import Container from './components/Container.js'
+import Section from './components/Section.js'
+import Balance from './components/Balance.js';
 
+const compountInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (1 + rate)
+  }
+  return Math.round(total)
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+  });
 function App() {
+  const [balance, setBalance] = useState('')
+  const handleSubmit = ( {deposit, contribution, years, rate } ) => {
+    const val = compountInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalance(formatter.format(val))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Section>
+        <Formik
+        initialValues={{
+          deposit: '',
+          contribution: '',
+          years: '',
+          rate: '',
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={Yup.object({
+          deposit: Yup.number()
+            .required('Requerido')
+            .typeError('Debe ser un número')
+            .positive('Solo números positivos'),
+          contribution: Yup.number()
+            .required('Requerido')
+            .typeError('Debe ser un número')
+            .positive('Solo números positivos'),
+          years: Yup.number()
+            .required('Requerido')
+            .typeError('Debe ser un número')
+            .positive('Solo números positivos'),
+          rate: Yup.number()
+            .required('Requerido')
+            .typeError('Debe ser un número')
+            .min(0, 'El valor mínimo es 1')
+            .max(1, 'El valor máximo es 1')
+            .positive('Solo números positivos'),
+        })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form>
+            <Input name="deposit" label="Depósito inicial" />
+            <Input name="contribution" label="Contribución anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Interés estimado" />
+            <Button type="submit">Calcular</Button>
+          </Form>
+        </Formik>
+        {balance ? <Balance>Tu balance es de {balance}</Balance> : null}
+      </Section>
+    </Container>
   );
 }
 
---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Balance = styled.span`
+    font-size: 36px;
+    margin-top: 12px;
+    displat: inline-block;
+    color: #000;
+`;
+
+export default Balance;
